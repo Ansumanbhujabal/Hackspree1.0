c891b9f47ea7c486c71a303e88f5e3abc9288dbf
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2023-07-02 19:52:51 -0500

    routes added for landing page and community calendar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Home from "./pages/Home";
+import UserHome from "./pages/UserHome";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import "./App.css";
+import Layout from "./pages/Layout/Layout";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import CommunityCal from "./pages/CommunityCal/CommunityCal";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -24,12 +27,27 @@ function App() {
   }, []);
 
   return (
-    <div className="container">
+    <div className="App">
+    
       <Routes>
+      <Route
+          exact
+          path="/"
+          element={<Layout />}>
+            
         <Route
           exact
           path="/"
-          element={user ? <Home user={user} /> : <Navigate to="/login" />}
+          element={user ? <UserHome user={user} /> : <Navigate to="/home" />}
+        />
+        <Route
+          exact
+          path="/home"
+          element={user ? <Navigate to="/" /> : <LandingPage />}
+        />
+        <Route
+          path="/community-calendar"
+          element={user ? <CommunityCal/> : <LandingPage />}
         />
         <Route
           exact
@@ -40,8 +58,9 @@ function App() {
           path="/signup"
           element={user ? <Navigate to="/" /> : <Signup />}
         />
+        </Route>
       </Routes>
-    </div>
+      </div>
   );
 }
 
