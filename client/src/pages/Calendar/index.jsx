9cbac43ce3ebb96d343dd136730f87bfac3372cd
Andee Warren <119145298+andeewarren@users.noreply.github.com>
2023-07-11 15:00:04 -0400

    added more to calendar

--- client/src/pages/Calendar/index.jsx
+++ client/src/pages/Calendar/index.jsx
@@ -1,3 +1,5 @@
+import React, { Fragment, useMemo } from 'react'
+import PropTypes from 'prop-types'
 import { Calendar, luxonLocalizer } from 'react-big-calendar';
 import { DateTime, Settings } from 'luxon';
 
@@ -16,3 +18,28 @@ const MyCalendar = (props) => (
       />
     </div>
   )
+
+export default function Popup({ localizer }) {
+const defaultDate = useMemo(() => new Date(2015, 3, 1), [])
+return (
+    <Fragment>
+    {/* <DemoLink fileName="popup">
+        <strong>
+        Click the "+&#123;x&#125; more" link on any calendar day that cannot
+        fit all the days events to see an inline popup of all the events.
+        </strong>
+    </DemoLink> */}
+    <div className="height600">
+        <Calendar
+        defaultDate={defaultDate}
+        events={events}
+        localizer={localizer}
+        popup
+        />
+    </div>
+    </Fragment>
+)
+}
+Popup.propTypes = {
+localizer: PropTypes.instanceOf(DateLocalizer),
+}
